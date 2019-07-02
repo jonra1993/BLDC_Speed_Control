45029a2e48b70f18c9539e6e17d9754fdb7a262d
Jona <jon_ra@hotmail.es>
2019-07-02 10:03:36 -0500

    Add libraries

--- IR_Sensor/IR_Sensor.ino
+++ IR_Sensor/IR_Sensor.ino
@@ -1,19 +1,23 @@
 #include <Servo.h>
+#include "FC_51.h"
+#include "PID.h"
 
+#define IR_PIN 5
+#define LED_PIN 13
+#define MOTOR_PIN 9
 #define MAX_SIGNAL 2000
 #define MIN_SIGNAL 1000
 #define MOTOR_PIN 9
-int DELAY = 1000;
 
-Servo motor;
+FC_51 ir_sensor(IR_PIN);
 
 void setup() {
-
-
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-   
+  digitalWrite(LED_PIN,ir_sensor.read()); //white hight and black low
+  Serial.println(ir_sensor.read());
 
 }
  
